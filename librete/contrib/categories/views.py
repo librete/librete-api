8c5f9e8a790db3e172960043fccd3f91c493ee2e
Hristo Gatsinski <gatsinski@gmail.com>
2018-04-08 14:16:12 +0300

    Add `CategoryViewSet`

--- librete/contrib/categories/views.py
+++ librete/contrib/categories/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from librete.utils.permissions import IsAuthor
+from .models import Category
+from .serializers import CategorySerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    permission_classes = (IsAuthor,)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def get_queryset(self):
+        return Category.objects.all().filter(author=self.request.user)
