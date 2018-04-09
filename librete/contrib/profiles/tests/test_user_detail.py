76342908d33ab12463ce5efd0cf386ada3dba39f
Hristo Gatsinski <gatsinski@gmail.com>
2018-04-09 17:09:55 +0300

    Add `test_put_without_password` to `UserDetailTestCase`

--- librete/contrib/profiles/tests/test_user_detail.py
+++ librete/contrib/profiles/tests/test_user_detail.py
@@ -137,6 +137,24 @@ class UserDetailTestCase(APITestCase):
         # Test if username is changed
         self.assertEqual(user.username, self.user.username)
 
+    def test_put_without_password(self):
+        url = reverse('user-detail', args=[self.user.pk])
+
+        data = {
+            'username': 'richard.roe',
+            'email': 'richard.roe@example.com',
+            'first_name': 'Richard',
+            'last_name': 'Roe',
+        }
+
+        response = self.client.put(url,
+                                   data=data,
+                                   format='json',
+                                   HTTP_AUTHORIZATION=self.token)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertTrue('current_password' in response.json())
+
     def test_delete(self):
         url = reverse('user-detail', args=[self.user.pk])
 
