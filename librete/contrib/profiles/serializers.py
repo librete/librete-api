80ffe9647d012dddd9590ea073d747db480015b4
Hristo Gatsinski <gatsinski@gmail.com>
2018-04-09 15:26:46 +0300

    Add password hash

--- librete/contrib/profiles/serializers.py
+++ librete/contrib/profiles/serializers.py
@@ -24,9 +24,23 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('url',
                   'username',
                   'email',
+                  'password',
                   'first_name',
                   'last_name',
                   'categories',
                   'events',
                   'notes',
                   'tasks')
+        extra_kwargs = {
+            'password': {
+                'write_only': True
+            }
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
