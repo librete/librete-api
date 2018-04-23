1e22a2f2c694e54abd3244a8816c49ade8bcfa5f
Hristo Gatsinski <gatsinski@gmail.com>
2018-04-23 22:04:10 +0300

    Add `Django CORS Headers`

--- librete/settings.py
+++ librete/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'rest_framework',
     'oauth2_provider',
+    'corsheaders',
 
     # Project apps
     'librete.contrib.categories.apps.CategoriesConfig',
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
