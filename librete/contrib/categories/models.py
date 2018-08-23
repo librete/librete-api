f745b57907fe2afc3a1a34399e6d21e42dec0e74
Hristo Gatsinski <gatsinski@gmail.com>
2018-08-23 22:39:14 +0300

    Add `DefaultCategory` model

--- librete/contrib/categories/models.py
+++ librete/contrib/categories/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
+from parler.models import TranslatableModel, TranslatedFields
+
 from librete.utils.models import TimestampModel
 
 
@@ -19,6 +21,23 @@ class Category(TimestampModel):
         ordering = ['created_at']
         verbose_name = _('Category')
         verbose_name_plural = _('Categories')
+
+    def __str__(self):
+        return self.name
+
+class DefaultCategory(TranslatableModel, TimestampModel):
+    translations = TranslatedFields(
+        name=models.CharField(_('Name'),
+                              max_length=254),
+        description=models.CharField(_('Description'),
+                                     max_length=1000,
+                                     blank=True)
+    )
+
+    class Meta:
+        ordering = ['created_at']
+        verbose_name = _('Default category')
+        verbose_name_plural = _('Default categories')
 
     def __str__(self):
         return self.name
